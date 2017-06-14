413300508d5d26ce353283f913fcc04743b5c8b7
David <david.amison20@gmail.com>
2017-06-14 14:48:19 +0100

    Minor Changes

--- python/Pi_1/PiCam.py
+++ python/Pi_1/PiCam.py
@@ -8,7 +8,7 @@ import os
 
 def ensure_dir(file_path):
     #Creates a directory if it doesn't exist from a Path variable
-    if file_path.exists() == False:
+    if not file_path.exists():
         file_path.mkdir(parents=True)
     
 
@@ -34,14 +34,12 @@ class PiCam():
     
     def _cut_video(self, length, file_name):
         '''Records a video of the given length cur into segments'''
-        print('creating cut video-length: {}, cut: {}'.format(length,self.video_cut))
         camera = picamera.PiCamera(resolution=self.resolution, framerate=20)
         n = 1
         camera.start_recording('PiCam_Vid/{}_{}.h264'.format(file_name,n))
         total_time = 0
         while (total_time+self.video_cut)<= length:
             camera.wait_recording(self.video_cut)
-            print('splitting recording:',n)
             n += 1
             camera.split_recording('PiCam_Vid/{}_{}.h264'.format(file_name,n))
             total_time = total_time+self.video_cut
