a8ab595dfabc74042235982f1210095d6353cc4c
David <david.amison20@gmail.com>
2017-06-14 11:38:53 +0100

    Input Test

--- python/Pi_1/__init__.py
+++ python/Pi_1/__init__.py
@@ -13,19 +13,21 @@ import IMU_1
 #Setup all the pins on the Pi
 print('Setting up GPIO Pins')
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(35, GPIO.OUT)
+GPIO.setup(35, GPIO.IN)
 GPIO.setup(40, GPIO.OUT)
-GPIO.setup(12, GPIO.IN)
 
 #Flash some LED's
 for i in range(0,5):
     print('Flashing LEDs')
-    GPIO.output(35,GPIO.HIGH)
     GPIO.output(40,GPIO.LOW)
     time.sleep(2)
-    GPIO.output(35,GPIO.LOW)
     GPIO.output(40,GPIO.HIGH)
-    time.sleep(2)    
+    time.sleep(2)
+
+while not GPIO.input(35):
+    continue
+
+GPIO.output(40,GPIO.LOW)
 
 GPIO.cleanup()
 #TODO create REXUS class for communicating with ground
@@ -39,11 +41,11 @@ while 1:
     #TODO Communicate with ground before lift-off
     Message = 'Some data to send'
     Response = REXUS_Comm.communicate(Message)
-    break
+    
     #TODO Check for Software Test Mode- REXUS?
     #TODO Check for command to start camera    
     #TODO Check for Lift-off GPIO
-    
+    break
 
 
 #TODO Check for SOE- GPIO
